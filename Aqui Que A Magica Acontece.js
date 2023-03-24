eecc0b539493569c3da202e1fd37dd3d6b52835e
unknown <theopinheiro12@gmail.com>
2023-03-23 17:49:54 -0700

    mandar

---
+++ Aqui Que A Magica Acontece.js
@@ -0,0 +1,104 @@
+function enviar(){
+    let final = document.querySelector("#bottom")
+
+    let emailCordenador = document.querySelector("#nomeCordenador") 
+    let cordenadorEscolhido = emailCordenador.options[emailCordenador.selectedIndex].text
+
+    let email = escolherEmailIntitussional(cordenadorEscolhido)
+
+    
+
+    let disciplina = document.querySelector("#disciplina").value
+    
+    let sexoProfessor = document.querySelector("#sexoInstrutor")
+    let ProfessorOuProfessora = sexoProfessor.options[sexoProfessor.selectedIndex].text
+
+    let docente = OouA(ProfessorOuProfessora)
+
+    let NomeDocente = document.querySelector("#NomeDocente").value
+    let NomeDisciplina = document.querySelector("#disciplina").value
+
+    let Cursos = document.querySelector("#TecnicoEm")
+    let OpcoesCursos = Cursos.options[Cursos.selectedIndex].text
+
+    let Tec = QualTecnico(OpcoesCursos)
+
+    let horarioDiv = document.querySelector("#HoraAula")
+    let cargaHoraria = horarioDiv.options[horarioDiv.selectedIndex].text 
+
+    let DataInicio = document.querySelector("#DataInicio").value
+    let DataFim = document.querySelector("#DataFim").value
+
+
+    final.innerHTML = ` 
+    
+    <p> Para: ${email} </p>
+    
+    <h2> Assunto: Documentação ${docente} ${NomeDocente} da disciplina: ${NomeDisciplina} do ${Tec}  </h2>
+
+    <p> horario
+        <br>
+        Segue em Anexo a documentação ${docente}: <strong>${NomeDocente}</strong> para ministrar a disciplina: ${NomeDisciplina} (${cargaHoraria}) no periodo de <strong>${DataInicio}</strong> a <strong>${DataFim}</strong>
+        <br><br>
+        Atenciosamente
+
+
+    </p>
+    
+    `
+  
+
+}
+
+
+function OouA(instrutor){
+
+    if(instrutor === 'professor'){
+         return ProfessorOuProfessora = 'do Professor'
+   }else{
+    return ProfessorOuProfessora = 'da Professora'
+   }
+}
+
+function QualTecnico(NomeCurso){
+    if(NomeCurso === 'Geoprocessamento'){
+        return NomeCurso = 'Curso Tecnico em Geoprocessamento'
+    }else if (NomeCurso === 'Eletrotecnica'){
+        return NomeCurso = 'Curso Tecnico em Eletrotecnica'
+    }else if (NomeCurso === 'Nutrição'){
+        return NomeCurso = 'Curso Tecnico em Nutrição'
+    }else if (NomeCurso === 'Serviços Juridicos'){
+        return NomeCurso = 'Curso Tecnico em Serviços Juridicos'
+    }else if (NomeCurso === 'Gastronomia'){
+        return NomeCurso = 'Curso Tecnico em Gastronomia'
+    }else if (NomeCurso === '(RH)Recursos Humanos'){
+        return NomeCurso = 'Curso Tecnico em Recursos Humanos'
+    }else if (NomeCurso === 'Refrigeração'){
+        return NomeCurso = 'Curso Tecnico em Refrigeração'
+    }else if (NomeCurso === 'Enfermagem'){
+        return NomeCurso = 'Curso Tecnico em Enfermagem'
+    }else if (NomeCurso === 'Enfermagem'){
+        return NomeCurso = 'Curso Tecnico em Enfermagem'
+    }else{
+        return
+    }
+}
+
+function escolherEmailIntitussional(nome){
+    if (nome === 'Maria Emilia'){
+        return 'maria.freitas@edu.cetam.am.gov.br'
+    }else if (nome === 'Deborah Tavares'){
+        return 'deborah.tavares@edu.cetam.am.gov.br'
+}else if (nome === 'Ires Silva'){
+    return 'ires.silva@cetam.am.gov.br'
+}else if (nome === 'Mauro'){
+    return 'mauro.lima@edu.cetam.am.gov.br'
+}else if (nome === 'Greciane Rossetti'){
+    return 'greciane.rossetti@edu.cetam.am.gov.br'
+}
+
+
+
+
+}
+
